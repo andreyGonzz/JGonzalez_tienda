a00db30767837a413487abd0b88aa475f7449476
Jorge Andrey Gonzalez Ruiz <jorgeandreygonzalez2006@gmail.com>
2025-09-21 21:14:58 -0600

    Initialize Spring Boot project for TechShop
    Set up a new Spring Boot web application with Thymeleaf, basic configuration, and initial templates. Includes project structure, Maven configuration, static resources, and a simple test to verify context loading.

---
+++ TiendaWeb/src/main/java/TiendaTec/com/demo/TiendaTecApplication.java
@@ -0,0 +1,13 @@
+package TiendaTec.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaTecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaTecApplication.class, args);
+	}
+
+}
