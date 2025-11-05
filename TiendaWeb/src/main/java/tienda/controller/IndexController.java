b3c05d6f0c8b2fca83d02143018ad06c64a7ce20
Jorge Andrey Gonzalez Ruiz <jorgeandreygonzalez2006@gmail.com>
2025-11-04 21:26:53 -0600

    Commit semana 8 y 9

--- TiendaWeb/src/main/java/tienda/controller/IndexController.java
+++ TiendaWeb/src/main/java/tienda/controller/IndexController.java
@@ -9,17 +9,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
-    
-    // Las últimas versiones de Spring, recomiendan utilziar final y contructor en lugar de @autowired
+
+    // Las últimas versiones de Spring, recomiendan utilziar final y contructor en
+    // lugar de @autowired
     private final ProductoService productoService;
     private final CategoriaService categoriaService;
-    
+
     // (Spring inyecta automáticamente)
     public IndexController(ProductoService productoService, CategoriaService categoriaService) {
         this.productoService = productoService;
         this.categoriaService = categoriaService;
     }
-    
+
     @GetMapping("/")
     public String cargarPaginaInicio(Model model) {
         var lista = productoService.getProductos(true);
@@ -28,13 +29,13 @@ public class IndexController {
         model.addAttribute("categorias", categorias);
         return "/index";
     }
-    
+
     @GetMapping("/consultas/{idCategoria}")
     public String listado(@PathVariable("idCategoria") Integer idCategoria, Model model) {
         model.addAttribute("idCategoriaActual", idCategoria);
         var categoriaOptional = categoriaService.getCategoria(idCategoria);
         if (categoriaOptional.isEmpty()) {
-            //Puede ser que no se exista la categoria buscada...
+            // Puede ser que no se exista la categoria buscada...
             model.addAttribute("productos", java.util.Collections.emptyList());
         } else {
             var categoria = categoriaOptional.get();
